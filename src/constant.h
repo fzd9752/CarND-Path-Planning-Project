c322754d9cce9e208444d8c8ca08b2c7c0772c01
fzd9752 <fzd9752@msn.com>
2019-08-28 09:29:39 +0800

    cubic poly trajectories

---
+++ src/constant.h
@@ -0,0 +1,16 @@
+//
+// Created by YishuWang on 28/08/2019.
+//
+
+#ifndef PATH_PLANNING_CONSTANT_H
+#define PATH_PLANNING_CONSTANT_H
+
+
+#define MAX_SPEED 49.5
+
+#define MAX_ACC .224;
+
+
+
+
+#endif //PATH_PLANNING_CONSTANT_H
